1df829b5683cd161321088af4c65d9f25e1a89b4
Yadong Liu <liuyadong828@gmail.com>
2020-07-30 14:41:56 +0900

    add cmsc and test

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,62 @@
 
 using namespace Rcpp;
 
+// cmsc
+double cmsc(arma::vec x, arma::vec y, bool rescale, double xmin, double xmax, double ymin, double ymax, std::string comp);
+RcppExport SEXP _GCSM_cmsc(SEXP xSEXP, SEXP ySEXP, SEXP rescaleSEXP, SEXP xminSEXP, SEXP xmaxSEXP, SEXP yminSEXP, SEXP ymaxSEXP, SEXP compSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::vec >::type x(xSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type y(ySEXP);
+    Rcpp::traits::input_parameter< bool >::type rescale(rescaleSEXP);
+    Rcpp::traits::input_parameter< double >::type xmin(xminSEXP);
+    Rcpp::traits::input_parameter< double >::type xmax(xmaxSEXP);
+    Rcpp::traits::input_parameter< double >::type ymin(yminSEXP);
+    Rcpp::traits::input_parameter< double >::type ymax(ymaxSEXP);
+    Rcpp::traits::input_parameter< std::string >::type comp(compSEXP);
+    rcpp_result_gen = Rcpp::wrap(cmsc(x, y, rescale, xmin, xmax, ymin, ymax, comp));
+    return rcpp_result_gen;
+END_RCPP
+}
+// cmsc_sw
+arma::mat cmsc_sw(arma::mat x, arma::mat y, bool rescale, double xmin, double xmax, double ymin, double ymax, double ksize, bool globe, std::string comp);
+RcppExport SEXP _GCSM_cmsc_sw(SEXP xSEXP, SEXP ySEXP, SEXP rescaleSEXP, SEXP xminSEXP, SEXP xmaxSEXP, SEXP yminSEXP, SEXP ymaxSEXP, SEXP ksizeSEXP, SEXP globeSEXP, SEXP compSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat >::type x(xSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type y(ySEXP);
+    Rcpp::traits::input_parameter< bool >::type rescale(rescaleSEXP);
+    Rcpp::traits::input_parameter< double >::type xmin(xminSEXP);
+    Rcpp::traits::input_parameter< double >::type xmax(xmaxSEXP);
+    Rcpp::traits::input_parameter< double >::type ymin(yminSEXP);
+    Rcpp::traits::input_parameter< double >::type ymax(ymaxSEXP);
+    Rcpp::traits::input_parameter< double >::type ksize(ksizeSEXP);
+    Rcpp::traits::input_parameter< bool >::type globe(globeSEXP);
+    Rcpp::traits::input_parameter< std::string >::type comp(compSEXP);
+    rcpp_result_gen = Rcpp::wrap(cmsc_sw(x, y, rescale, xmin, xmax, ymin, ymax, ksize, globe, comp));
+    return rcpp_result_gen;
+END_RCPP
+}
+// cmsc_tw
+arma::mat cmsc_tw(arma::cube xxx, arma::cube yyy, bool rescale, double xmin, double xmax, double ymin, double ymax, std::string comp);
+RcppExport SEXP _GCSM_cmsc_tw(SEXP xxxSEXP, SEXP yyySEXP, SEXP rescaleSEXP, SEXP xminSEXP, SEXP xmaxSEXP, SEXP yminSEXP, SEXP ymaxSEXP, SEXP compSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::cube >::type xxx(xxxSEXP);
+    Rcpp::traits::input_parameter< arma::cube >::type yyy(yyySEXP);
+    Rcpp::traits::input_parameter< bool >::type rescale(rescaleSEXP);
+    Rcpp::traits::input_parameter< double >::type xmin(xminSEXP);
+    Rcpp::traits::input_parameter< double >::type xmax(xmaxSEXP);
+    Rcpp::traits::input_parameter< double >::type ymin(yminSEXP);
+    Rcpp::traits::input_parameter< double >::type ymax(ymaxSEXP);
+    Rcpp::traits::input_parameter< std::string >::type comp(compSEXP);
+    rcpp_result_gen = Rcpp::wrap(cmsc_tw(xxx, yyy, rescale, xmin, xmax, ymin, ymax, comp));
+    return rcpp_result_gen;
+END_RCPP
+}
 // gcsm
 double gcsm(arma::vec x, arma::vec y, bool rescale, double xmin, double xmax, double ymin, double ymax, std::string comp);
 RcppExport SEXP _GCSM_gcsm(SEXP xSEXP, SEXP ySEXP, SEXP rescaleSEXP, SEXP xminSEXP, SEXP xmaxSEXP, SEXP yminSEXP, SEXP ymaxSEXP, SEXP compSEXP) {
@@ -64,6 +120,9 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_GCSM_cmsc", (DL_FUNC) &_GCSM_cmsc, 8},
+    {"_GCSM_cmsc_sw", (DL_FUNC) &_GCSM_cmsc_sw, 10},
+    {"_GCSM_cmsc_tw", (DL_FUNC) &_GCSM_cmsc_tw, 8},
     {"_GCSM_gcsm", (DL_FUNC) &_GCSM_gcsm, 8},
     {"_GCSM_gcsm_sw", (DL_FUNC) &_GCSM_gcsm_sw, 10},
     {"_GCSM_gcsm_tw", (DL_FUNC) &_GCSM_gcsm_tw, 8},
